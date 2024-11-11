69869fab4aa2d82a4170a8f6e1d088ab055bc9a5
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-11-11 13:34:13 -0600

    redo task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.arv[1])).json()
     todos = requests.get(url + "todos?userID={}".format(sys.argv[1])).json()
 
-    completed = [todo.get("title") for todo in todos
-                if todo.get("completed") is True]
+    completed = [todo.get("title") for todo in todos if todo.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     for task in completed:
