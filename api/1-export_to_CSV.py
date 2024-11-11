081c776c9a9db6de78dfd28cce169b7b15c6e6b6
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-11-11 13:53:45 -0600

    task1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Getting data from API"""
+
+
+import requests
+import sys
+
+
+def main():
+    """According to user_id, show information
+    """
+    user_id = sys.argv[1]
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    todos = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(
+        user_id)
+    name = requests.get(user).json().get('name')
+    request_todo = requests.get(todos).json()
+    with open('{}.csv'.format(user_id), 'w+') as file:
+        for todo in request_todo:
+            info = '"{}","{}","{}","{}"\n'.format(
+                user_id, name, todo.get('completed'), todo.get('title'))
+            file.write(info)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main()
