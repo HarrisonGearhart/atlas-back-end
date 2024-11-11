bd3eb2111ec65063627eadc3675e44f000a5985b
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-11-11 13:32:34 -0600

    redo task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     completed = [todo.get("title") for todo in todos
         if todo.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name") len(completed), len(todos)))
+        user.get("name"), len(completed), len(todos)))
     for task in completed:
         print("\t {}".format(task))
